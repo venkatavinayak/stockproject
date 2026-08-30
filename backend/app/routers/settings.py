cd0d249729057e1c9ee44c8a3e67b8e5432d776c
venkata vinayak <venkatavinayak1@gmail.com>
2026-08-30 20:56:45 +0530

    feat: allow cashiers (workers) to retrieve store settings

--- backend/app/routers/settings.py
+++ backend/app/routers/settings.py
@@ -3,14 +3,14 @@ from beanie import PydanticObjectId
 
 from backend.app.models.settings import StoreSettings
 from backend.app.schemas.settings import StoreSettingsUpdate, StoreSettingsResponse
-from backend.app.auth.deps import get_current_admin
+from backend.app.auth.deps import get_current_admin, get_current_user
 from backend.app.models.user import User
 
 router = APIRouter(prefix="/settings", tags=["Store Settings"])
 
 @router.get("", response_model=StoreSettingsResponse)
 async def get_settings(
-    current_user: User = Depends(get_current_admin)
+    current_user: User = Depends(get_current_user)
 ):
     owner_username = current_user.owner
     settings = await StoreSettings.find_one(StoreSettings.owner_username == owner_username)
